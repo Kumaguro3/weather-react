dc8af09a0d8746f763bd5721b0f2004522adb486
Kumaguro3 <lily.sergent@gmail.com>
2022-09-20 18:25:24 +0900

    Updated bootstrap and npm

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Weather from "./Weather.js";
 export default function App() {
   return (
     <div className="App">
-      <Weather />
-      <footer>
-        This project has been built by{" "}
-        <a href="https://github.com/Kumaguro3/weather-react">Sergent Julie</a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project has been built by{" "}
+          <a href="https://github.com/Kumaguro3/weather-react">Sergent Julie</a>
+        </footer>
+      </div>
     </div>
   );
 }
