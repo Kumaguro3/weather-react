0ecd52f1f69d59401fa1a375a856ebb98d7bbb5d
Kumaguro3 <lily.sergent@gmail.com>
2022-09-20 14:28:11 +0900

    Imported Weather project into React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Weather from "./Weather.js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello React!</h1>
+      <Weather />
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,75 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  let weatherData = {
+    city: "Tokyo",
+    day: "Monday",
+    time: "10:00",
+    temperature: "19",
+    description: "Clear",
+    humidity: "60",
+    wind: "5",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+  };
+
+  return (
+    <div className="Weather">
+      <div className="container">
+        <div className="btn-group" margin="10px">
+          <form id="search-form">
+            <input
+              type="text"
+              placeholder="Enter a city"
+              id="search-text-input"
+            />
+            <button type="submit" class="btn btn-primary">
+              Go!
+            </button>
+          </form>
+          <button type="click" class="btn btn-primary" id="current-location">
+            Current
+          </button>
+        </div>
+
+        <h1 className="main-city" id="main-city">
+          {weatherData.city}
+        </h1>
+        <h4 className="dayOfWeek">{weatherData.day}</h4>
+        <h4 className="time">{weatherData.time}</h4>
+
+        <img src={weatherData.imgUrl} alt={weatherData.description} id="icon" />
+        <br />
+        <div className="container2">
+          <h3 className="temperature" id="main-temperature">
+            {weatherData.temperature}
+          </h3>
+          <a href="https://www.google.com/" id="celsius-link" class="active">
+            °C
+          </a>{" "}
+          |
+          <a href="https://www.google.com/" id="fahren-link">
+            °F
+          </a>
+        </div>
+      </div>
+      <div className="container">
+        <div className="description">
+          <h4 id="description">{weatherData.description}</h4>
+        </div>
+
+        <div className="col-md-12 col-sm-3 col-xs-3 side-weather-info">
+          <h6 className="humidity">
+            Humidity: <spam id="humidity">{weatherData.humidity}</spam>%
+          </h6>
+        </div>
+
+        <div className="col-md-12 col-sm-3 col-xs-3 side-weather-info">
+          <h6 className="wind">
+            Wind: <spam id="wind"> {weatherData.wind}</spam>km/h
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
+}
