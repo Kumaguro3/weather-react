32898a63840e89494ec276858ed1ee78d6d8dde3
Kumaguro3 <lily.sergent@gmail.com>
2022-09-27 22:34:32 +0900

    Separated Footer from App folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React from "react";
+import Footer from "./Footer.js";
 import Weather from "./Weather.js";
 
 export default function App() {
@@ -8,10 +9,7 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Tokyo" />
       </div>
-      <footer>
-        This app has been built by{" "}
-        <a href="https://github.com/Kumaguro3/weather-react">Sergent Julie</a>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <footer>
+        This app has been built by{" "}
+        <a href="https://github.com/Kumaguro3/weather-react">Sergent Julie</a>
+      </footer>
+    </div>
+  );
+}
